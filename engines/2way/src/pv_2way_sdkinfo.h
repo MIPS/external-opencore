51cc54e552715598e933bd15e062ee8286c683bc
PacketVideo CM <engbuild@pv.com>
2009-09-03 11:19:52 -0700

    Update sdkinfo header files and playerdriver with CORE_7.505.1.1 version

--- engines/2way/src/pv_2way_sdkinfo.h
+++ engines/2way/src/pv_2way_sdkinfo.h
@@ -21,7 +21,7 @@
 // This header file is automatically generated at build-time
 // *** OFFICIAL RELEASE INFO -- Will not auto update
 
-#define PV2WAY_ENGINE_SDKINFO_LABEL "937493"
-#define PV2WAY_ENGINE_SDKINFO_DATE 0x20090828
+#define PV2WAY_ENGINE_SDKINFO_LABEL "CORE_7.505.1.1"
+#define PV2WAY_ENGINE_SDKINFO_DATE 0x20090829
 
 #endif //PV_2WAY_SDKINFO_H_INCLUDED
--- engines/player/src/pv_player_sdkinfo.h
+++ engines/player/src/pv_player_sdkinfo.h
@@ -21,7 +21,7 @@
 // This header file is automatically generated at build-time
 // *** OFFICIAL RELEASE INFO -- Will not auto update
 
-#define PVPLAYER_ENGINE_SDKINFO_LABEL "937493"
-#define PVPLAYER_ENGINE_SDKINFO_DATE 0x20090828
+#define PVPLAYER_ENGINE_SDKINFO_LABEL "CORE_7.505.1.1"
+#define PVPLAYER_ENGINE_SDKINFO_DATE 0x20090829
 
 #endif //PV_PLAYER_SDKINFO_H_INCLUDED
