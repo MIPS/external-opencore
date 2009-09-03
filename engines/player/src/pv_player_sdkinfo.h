21b621b408d4bc915f297b0f6fe64417f41f929e
PacketVideo CM <engbuild@pv.com>
2009-09-03 11:19:47 -0700

    Remove unneeded makefile segments of streaming manager from PV build system

--- engines/player/src/pv_player_sdkinfo.h
+++ engines/player/src/pv_player_sdkinfo.h
@@ -21,7 +21,7 @@
 // This header file is automatically generated at build-time
 // *** OFFICIAL RELEASE INFO -- Will not auto update
 
-#define PVPLAYER_ENGINE_SDKINFO_LABEL "CORE_7.504.1.1"
-#define PVPLAYER_ENGINE_SDKINFO_DATE 0x20090822
+#define PVPLAYER_ENGINE_SDKINFO_LABEL "913739"
+#define PVPLAYER_ENGINE_SDKINFO_DATE 0x20090813
 
 #endif //PV_PLAYER_SDKINFO_H_INCLUDED
