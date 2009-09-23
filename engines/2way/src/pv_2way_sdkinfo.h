176a3953c058932617d0b83be2254d2886241c9c
PacketVideo CM <engbuild@pv.com>
2009-09-23 16:24:28 -0700

    RIO-7471: PV OMX component returns invalid supported profile/level pair.

--- engines/2way/src/pv_2way_sdkinfo.h
+++ engines/2way/src/pv_2way_sdkinfo.h
@@ -21,7 +21,7 @@
 // This header file is automatically generated at build-time
 // *** OFFICIAL RELEASE INFO -- Will not auto update
 
-#define PV2WAY_ENGINE_SDKINFO_LABEL "965391"
-#define PV2WAY_ENGINE_SDKINFO_DATE 0x20090915
+#define PV2WAY_ENGINE_SDKINFO_LABEL "966966"
+#define PV2WAY_ENGINE_SDKINFO_DATE 0x20090916
 
 #endif //PV_2WAY_SDKINFO_H_INCLUDED
