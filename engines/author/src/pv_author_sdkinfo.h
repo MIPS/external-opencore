7dfe6241407398c33a1a83793dec608aa4221ece
PacketVideo CM <engbuild@pv.com>
2009-09-23 16:24:30 -0700

    CORE_7.509.1.1 version updates for sdkinfo header files and playerdriver.  Documentation updates.

--- engines/author/src/pv_author_sdkinfo.h
+++ engines/author/src/pv_author_sdkinfo.h
@@ -21,7 +21,7 @@
 // This header file is automatically generated at build-time
 // *** OFFICIAL RELEASE INFO -- Will not auto update
 
-#define PVAUTHOR_ENGINE_SDKINFO_LABEL "973214"
-#define PVAUTHOR_ENGINE_SDKINFO_DATE 0x20090918
+#define PVAUTHOR_ENGINE_SDKINFO_LABEL "CORE_7.509.1.1"
+#define PVAUTHOR_ENGINE_SDKINFO_DATE 0x20090919
 
 #endif //PV_AUTHOR_SDKINFO_H_INCLUDED
