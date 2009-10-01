1499f67c02c3dffbc96870751c0b112d6a1fdbd6
PacketVideo CM <engbuild@pv.com>
2009-10-01 10:41:08 -0700

    RIO-7533: Send correct YUV format to the VMIO instead of PVMF_MIME_FORMAT_UNKNOWN.

--- engines/2way/src/pv_2way_sdkinfo.h
+++ engines/2way/src/pv_2way_sdkinfo.h
@@ -21,7 +21,7 @@
 // This header file is automatically generated at build-time
 // *** OFFICIAL RELEASE INFO -- Will not auto update
 
-#define PV2WAY_ENGINE_SDKINFO_LABEL "979032"
+#define PV2WAY_ENGINE_SDKINFO_LABEL "979327"
 #define PV2WAY_ENGINE_SDKINFO_DATE 0x20090923
 
 #endif //PV_2WAY_SDKINFO_H_INCLUDED
