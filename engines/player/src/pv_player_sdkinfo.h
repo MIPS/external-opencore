d2fb1c6c3004c1284401419648636dd032b9ef59
PacketVideo CM <engbuild@pv.com>
2009-10-07 22:45:38 -0700

    RIO-5144: Update to PV internal build system

--- engines/player/src/pv_player_sdkinfo.h
+++ engines/player/src/pv_player_sdkinfo.h
@@ -21,7 +21,7 @@
 // This header file is automatically generated at build-time
 // *** OFFICIAL RELEASE INFO -- Will not auto update
 
-#define PVPLAYER_ENGINE_SDKINFO_LABEL "993673"
-#define PVPLAYER_ENGINE_SDKINFO_DATE 0x20090930
+#define PVPLAYER_ENGINE_SDKINFO_LABEL "993887"
+#define PVPLAYER_ENGINE_SDKINFO_DATE 0x20091001
 
 #endif //PV_PLAYER_SDKINFO_H_INCLUDED
