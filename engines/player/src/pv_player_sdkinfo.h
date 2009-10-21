81f0ba947cce86df35e07fbde29115601ee4edea
PacketVideo CM <engbuild@pv.com>
2009-10-21 09:46:25 -0700

    RIO-7719: Treat OMX_EventError with OMX_ErrorInvalidState as if OMX client get an OMX_EventCmdComplete indicating state transition failure

--- engines/player/src/pv_player_sdkinfo.h
+++ engines/player/src/pv_player_sdkinfo.h
@@ -21,7 +21,7 @@
 // This header file is automatically generated at build-time
 // *** OFFICIAL RELEASE INFO -- Will not auto update
 
-#define PVPLAYER_ENGINE_SDKINFO_LABEL "1015092"
-#define PVPLAYER_ENGINE_SDKINFO_DATE 0x20091012
+#define PVPLAYER_ENGINE_SDKINFO_LABEL "1016092"
+#define PVPLAYER_ENGINE_SDKINFO_DATE 0x20091013
 
 #endif //PV_PLAYER_SDKINFO_H_INCLUDED
