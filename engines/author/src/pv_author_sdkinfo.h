55932f3724df77c6de3f505dd41aced407e614d1
PacketVideo CM <engbuild@pv.com>
2009-10-21 09:46:39 -0700

    RIO-7725: Add CTOR in RepositionRequestStruct to init the members

--- engines/author/src/pv_author_sdkinfo.h
+++ engines/author/src/pv_author_sdkinfo.h
@@ -21,7 +21,7 @@
 // This header file is automatically generated at build-time
 // *** OFFICIAL RELEASE INFO -- Will not auto update
 
-#define PVAUTHOR_ENGINE_SDKINFO_LABEL "1021968"
+#define PVAUTHOR_ENGINE_SDKINFO_LABEL "1022083"
 #define PVAUTHOR_ENGINE_SDKINFO_DATE 0x20091015
 
 #endif //PV_AUTHOR_SDKINFO_H_INCLUDED
