a8b60cfdd78ae432f8014cc871a9185f6cafd3bb
PacketVideo CM <engbuild@pv.com>
2009-10-21 09:46:24 -0700

    RIO-7460: Internal build system modification for prebuilt libraries.

--- engines/player/src/pv_player_sdkinfo.h
+++ engines/player/src/pv_player_sdkinfo.h
@@ -21,7 +21,7 @@
 // This header file is automatically generated at build-time
 // *** OFFICIAL RELEASE INFO -- Will not auto update
 
-#define PVPLAYER_ENGINE_SDKINFO_LABEL "1015080"
+#define PVPLAYER_ENGINE_SDKINFO_LABEL "1015092"
 #define PVPLAYER_ENGINE_SDKINFO_DATE 0x20091012
 
 #endif //PV_PLAYER_SDKINFO_H_INCLUDED
