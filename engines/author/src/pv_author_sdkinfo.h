0a9c4ea893877c67e061302233b7ea59542ef356
PacketVideo CM <engbuild@pv.com>
2009-11-04 09:40:52 -0800

    RIO-7566: Modifiying pvme unit test content specification

--- engines/author/src/pv_author_sdkinfo.h
+++ engines/author/src/pv_author_sdkinfo.h
@@ -21,7 +21,7 @@
 // This header file is automatically generated at build-time
 // *** OFFICIAL RELEASE INFO -- Will not auto update
 
-#define PVAUTHOR_ENGINE_SDKINFO_LABEL "CORE_8.000.1.1_RC3"
-#define PVAUTHOR_ENGINE_SDKINFO_DATE 0x20091024
+#define PVAUTHOR_ENGINE_SDKINFO_LABEL "1044440"
+#define PVAUTHOR_ENGINE_SDKINFO_DATE 0x20091027
 
 #endif //PV_AUTHOR_SDKINFO_H_INCLUDED
