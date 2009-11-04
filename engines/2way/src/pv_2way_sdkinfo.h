1b9f347512554c78ff9a745fc3e48f1c4aaaaf28
PacketVideo CM <engbuild@pv.com>
2009-11-04 09:40:54 -0800

    RIO-7800: Fix library dependecies in streaming manager node.

--- engines/2way/src/pv_2way_sdkinfo.h
+++ engines/2way/src/pv_2way_sdkinfo.h
@@ -21,7 +21,7 @@
 // This header file is automatically generated at build-time
 // *** OFFICIAL RELEASE INFO -- Will not auto update
 
-#define PV2WAY_ENGINE_SDKINFO_LABEL "1044756"
+#define PV2WAY_ENGINE_SDKINFO_LABEL "1045419"
 #define PV2WAY_ENGINE_SDKINFO_DATE 0x20091027
 
 #endif //PV_2WAY_SDKINFO_H_INCLUDED
