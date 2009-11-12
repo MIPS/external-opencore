666d4ba0af45be35bc3be5ba077eb1fee6cd30e8
PacketVideo CM <engbuild@pv.com>
2009-11-12 12:46:27 -0800

    RIO-7812: Improvements in project file creation

--- engines/author/src/pv_author_sdkinfo.h
+++ engines/author/src/pv_author_sdkinfo.h
@@ -21,7 +21,7 @@
 // This header file is automatically generated at build-time
 // *** OFFICIAL RELEASE INFO -- Will not auto update
 
-#define PVAUTHOR_ENGINE_SDKINFO_LABEL "1062187"
+#define PVAUTHOR_ENGINE_SDKINFO_LABEL "1062335"
 #define PVAUTHOR_ENGINE_SDKINFO_DATE 0x20091103
 
 #endif //PV_AUTHOR_SDKINFO_H_INCLUDED
