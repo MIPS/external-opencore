3a849729baf9dcbb95ad6a8f7f9e8a97100a4e8b
PacketVideo CM <engbuild@pv.com>
2009-11-12 12:46:34 -0800

    RIO-7866: Update ChangeLog for OpenCORE 2.06 (CORE_8.000.1.1)

--- engines/author/src/pv_author_sdkinfo.h
+++ engines/author/src/pv_author_sdkinfo.h
@@ -21,7 +21,7 @@
 // This header file is automatically generated at build-time
 // *** OFFICIAL RELEASE INFO -- Will not auto update
 
-#define PVAUTHOR_ENGINE_SDKINFO_LABEL "1071339"
+#define PVAUTHOR_ENGINE_SDKINFO_LABEL "1071392"
 #define PVAUTHOR_ENGINE_SDKINFO_DATE 0x20091108
 
 #endif //PV_AUTHOR_SDKINFO_H_INCLUDED
