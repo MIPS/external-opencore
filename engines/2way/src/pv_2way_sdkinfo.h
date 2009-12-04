dcea3565c2b438ddd6c4df2c13d80eab8115ced6
PacketVideo CM <engbuild@pv.com>
2009-12-04 11:48:47 -0800

    RIO-8093: AMR file parser improvements

--- engines/2way/src/pv_2way_sdkinfo.h
+++ engines/2way/src/pv_2way_sdkinfo.h
@@ -21,7 +21,7 @@
 // This header file is automatically generated at build-time
 // *** OFFICIAL RELEASE INFO -- Will not auto update
 
-#define PV2WAY_ENGINE_SDKINFO_LABEL "1106601"
+#define PV2WAY_ENGINE_SDKINFO_LABEL "1107411"
 #define PV2WAY_ENGINE_SDKINFO_DATE 0x20091124
 
 #endif //PV_2WAY_SDKINFO_H_INCLUDED
