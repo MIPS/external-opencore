c5de01d725b9a586f50aee3628e67f503c7b51f9
PacketVideo CM <engbuild@pv.com>
2009-12-10 12:40:31 -0800

    RIO-8188: Early and Late Sync Margins changed for better AV Sync

--- engines/author/src/pv_author_sdkinfo.h
+++ engines/author/src/pv_author_sdkinfo.h
@@ -21,7 +21,7 @@
 // This header file is automatically generated at build-time
 // *** OFFICIAL RELEASE INFO -- Will not auto update
 
-#define PVAUTHOR_ENGINE_SDKINFO_LABEL "1127733"
+#define PVAUTHOR_ENGINE_SDKINFO_LABEL "1127804"
 #define PVAUTHOR_ENGINE_SDKINFO_DATE 0x20091207
 
 #endif //PV_AUTHOR_SDKINFO_H_INCLUDED
