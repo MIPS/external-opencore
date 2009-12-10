9698c014b657425f6220d8b8a076741e0621ed70
PacketVideo CM <engbuild@pv.com>
2009-12-10 12:40:29 -0800

    RIO-7973: Fix for running a test number multiple times in makefile target for running parallel player engine tests (minor additional change 2)

--- engines/2way/src/pv_2way_sdkinfo.h
+++ engines/2way/src/pv_2way_sdkinfo.h
@@ -21,7 +21,7 @@
 // This header file is automatically generated at build-time
 // *** OFFICIAL RELEASE INFO -- Will not auto update
 
-#define PV2WAY_ENGINE_SDKINFO_LABEL "1122320"
+#define PV2WAY_ENGINE_SDKINFO_LABEL "1122345"
 #define PV2WAY_ENGINE_SDKINFO_DATE 0x20091203
 
 #endif //PV_2WAY_SDKINFO_H_INCLUDED
