990dd4ba7fc45dd190d6c01639ce4bc4fa2960d6
PacketVideo CM <engbuild@pv.com>
2009-12-15 09:48:23 -0800

    RIO-8256: Add dependency of librt to Android x86 builds

--- Android_system_extras.mk
+++ Android_system_extras.mk
@@ -2,7 +2,7 @@
 LOCAL_LDLIBS := -lpthread
 
 ifeq ($(TARGET_OS)-$(TARGET_SIMULATOR),linux-true)
-LOCAL_LDLIBS += -ldl
+LOCAL_LDLIBS += -ldl -lrt
 endif
 ifneq ($(TARGET_SIMULATOR),true)
 LOCAL_SHARED_LIBRARIES += libdl
