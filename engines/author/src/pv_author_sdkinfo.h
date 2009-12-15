04d51f1a43b82a303c2d9eef87ab41c8f07f304c
PacketVideo CM <engbuild@pv.com>
2009-12-15 09:48:22 -0800

    RIO-7973: Fix for running a test number multiple times in makefile target for running parallel player engine tests

--- engines/author/src/pv_author_sdkinfo.h
+++ engines/author/src/pv_author_sdkinfo.h
@@ -21,7 +21,7 @@
 // This header file is automatically generated at build-time
 // *** OFFICIAL RELEASE INFO -- Will not auto update
 
-#define PVAUTHOR_ENGINE_SDKINFO_LABEL "1133509"
-#define PVAUTHOR_ENGINE_SDKINFO_DATE 0x20091209
+#define PVAUTHOR_ENGINE_SDKINFO_LABEL "1135368"
+#define PVAUTHOR_ENGINE_SDKINFO_DATE 0x20091210
 
 #endif //PV_AUTHOR_SDKINFO_H_INCLUDED
--- engines/player/src/pv_player_sdkinfo.h
+++ engines/player/src/pv_player_sdkinfo.h
@@ -21,7 +21,7 @@
 // This header file is automatically generated at build-time
 // *** OFFICIAL RELEASE INFO -- Will not auto update
 
-#define PVPLAYER_ENGINE_SDKINFO_LABEL "1133509"
-#define PVPLAYER_ENGINE_SDKINFO_DATE 0x20091209
+#define PVPLAYER_ENGINE_SDKINFO_LABEL "1135368"
+#define PVPLAYER_ENGINE_SDKINFO_DATE 0x20091210
 
 #endif //PV_PLAYER_SDKINFO_H_INCLUDED
