ab3772abf41eecee126f2095c2b5cb1d79377f51
PacketVideo CM <engbuild@pv.com>
2010-01-15 14:17:32 -0800

    CORE_8.107.1.1 versioning and documentation updates.

--- engines/player/src/pv_player_sdkinfo.h
+++ engines/player/src/pv_player_sdkinfo.h
@@ -21,7 +21,7 @@
 // This header file is automatically generated at build-time
 // *** OFFICIAL RELEASE INFO -- Will not auto update
 
-#define PVPLAYER_ENGINE_SDKINFO_LABEL "1174364"
+#define PVPLAYER_ENGINE_SDKINFO_LABEL "CORE_8.107.1.1"
 #define PVPLAYER_ENGINE_SDKINFO_DATE 0x20100111
 
 #endif //PV_PLAYER_SDKINFO_H_INCLUDED
