b8d40710df66c40fadfd71a9e593eb1e07ba74b0
PacketVideo CM <engbuild@pv.com>
2010-01-26 14:07:43 -0800

    RIO-7259: Some CML2 rules related to so_name can create errors with "n" symbol used as module name

--- engines/author/src/pv_author_sdkinfo.h
+++ engines/author/src/pv_author_sdkinfo.h
@@ -21,7 +21,7 @@
 // This header file is automatically generated at build-time
 // *** OFFICIAL RELEASE INFO -- Will not auto update
 
-#define PVAUTHOR_ENGINE_SDKINFO_LABEL "1180099"
+#define PVAUTHOR_ENGINE_SDKINFO_LABEL "1180621"
 #define PVAUTHOR_ENGINE_SDKINFO_DATE 0x20100113
 
 #endif //PV_AUTHOR_SDKINFO_H_INCLUDED
