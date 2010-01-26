36d8072fa2dc4fb9797392a8d26cf6966ffcde18
PacketVideo CM <engbuild@pv.com>
2010-01-26 14:07:36 -0800

    RIO-8413: Check the iSocketRequestAO before use

--- oscl/oscl/osclio/src/oscl_socket_method.h
+++ oscl/oscl/osclio/src/oscl_socket_method.h
@@ -1,5 +1,5 @@
 /* ------------------------------------------------------------------
- * Copyright (C) 1998-2009 PacketVideo
+ * Copyright (C) 1998-2010 PacketVideo
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -186,12 +186,14 @@ inline void OsclSocketMethod::CancelMethod()
     //cancel the timeout if any
     Abort();
     //cancel the request to the socket server.
-    iSocketRequestAO->DoCancel();
+    if (iSocketRequestAO)
+        iSocketRequestAO->DoCancel();
 }
 
 inline void OsclSocketMethod::MethodDone()
 {
-    iSocketRequestAO->Abort();
+    if (iSocketRequestAO)
+        iSocketRequestAO->Abort();
 }
 
 #endif
