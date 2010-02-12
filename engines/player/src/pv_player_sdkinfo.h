f5f903314b722fabdc44359fd8bdd9601d09086f
PacketVideo CM <engbuild@pv.com>
2010-02-11 18:47:23 -0800

    RIO-8472: 2way's h223LCtest and h223test are failing.

--- engines/player/src/pv_player_sdkinfo.h
+++ engines/player/src/pv_player_sdkinfo.h
@@ -21,7 +21,7 @@
 // This header file is automatically generated at build-time
 // *** OFFICIAL RELEASE INFO -- Will not auto update
 
-#define PVPLAYER_ENGINE_SDKINFO_LABEL "1241969"
+#define PVPLAYER_ENGINE_SDKINFO_LABEL "1242378"
 #define PVPLAYER_ENGINE_SDKINFO_DATE 0x20100202
 
 #endif //PV_PLAYER_SDKINFO_H_INCLUDED
--- protocols/systems/tools/general/common/src/pvmf_buffer_data_sink.cpp
+++ protocols/systems/tools/general/common/src/pvmf_buffer_data_sink.cpp
@@ -1,5 +1,5 @@
 /* ------------------------------------------------------------------
- * Copyright (C) 1998-2009 PacketVideo
+ * Copyright (C) 1998-2010 PacketVideo
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -27,6 +27,7 @@ OSCL_EXPORT_REF PVMFBufferDataSink::PVMFBufferDataSink(int32 aPortTag)
 
 OSCL_EXPORT_REF PVMFBufferDataSink::~PVMFBufferDataSink()
 {
+    SetActivityHandler(NULL);
 }
 
 void PVMFBufferDataSink::HandlePortActivity(const PVMFPortActivity &aActivity)
