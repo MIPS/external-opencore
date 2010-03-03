e0e59b887ec4a5b80b5b66817f43f869e48d9713
PacketVideo CM <engbuild@pv.com>
2010-03-02 16:25:53 -0800

    RIO-8712: Music app crashes and gallery application is not present on android-2.1_r1 emulator

--- engines/player/src/pv_player_sdkinfo.h
+++ engines/player/src/pv_player_sdkinfo.h
@@ -21,7 +21,7 @@
 // This header file is automatically generated at build-time
 // *** OFFICIAL RELEASE INFO -- Will not auto update
 
-#define PVPLAYER_ENGINE_SDKINFO_LABEL "CORE_8.502.1.1"
-#define PVPLAYER_ENGINE_SDKINFO_DATE 0x20100220
+#define PVPLAYER_ENGINE_SDKINFO_LABEL "1276494"
+#define PVPLAYER_ENGINE_SDKINFO_DATE 0x20100222
 
 #endif //PV_PLAYER_SDKINFO_H_INCLUDED
