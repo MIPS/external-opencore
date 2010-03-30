bacbbe654ba809b83a4bf41fdaa9e000bb730fdd
PacketVideo CM <engbuild@pv.com>
2010-03-30 14:58:51 -0700

    RIO-8714: Protect oscl file native seek and read operations on the same file handle
    Change-Id: If197cd120345b38ce23008fdb6ec37fdf02d40d3

--- engines/author/src/pv_author_sdkinfo.h
+++ engines/author/src/pv_author_sdkinfo.h
@@ -21,7 +21,7 @@
 // This header file is automatically generated at build-time
 // *** OFFICIAL RELEASE INFO -- Will not auto update
 
-#define PVAUTHOR_ENGINE_SDKINFO_LABEL "1338246"
+#define PVAUTHOR_ENGINE_SDKINFO_LABEL "1338484"
 #define PVAUTHOR_ENGINE_SDKINFO_DATE 0x20100323
 
 #endif //PV_AUTHOR_SDKINFO_H_INCLUDED
