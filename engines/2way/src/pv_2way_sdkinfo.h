7357a438dc921042a7f6a80d3d6fb3d835f7a9e4
PacketVideo CM <engbuild@pv.com>
2010-03-30 14:58:55 -0700

    RIO-8906: AAC-lib  Compiler bug on Android build creates the wrong assembly code
    Change-Id: If139bb620b12611aa47575ac6c15003be282d196

--- engines/2way/src/pv_2way_sdkinfo.h
+++ engines/2way/src/pv_2way_sdkinfo.h
@@ -21,7 +21,7 @@
 // This header file is automatically generated at build-time
 // *** OFFICIAL RELEASE INFO -- Will not auto update
 
-#define PV2WAY_ENGINE_SDKINFO_LABEL "1344550"
+#define PV2WAY_ENGINE_SDKINFO_LABEL "1345068"
 #define PV2WAY_ENGINE_SDKINFO_DATE 0x20100326
 
 #endif //PV_2WAY_SDKINFO_H_INCLUDED
