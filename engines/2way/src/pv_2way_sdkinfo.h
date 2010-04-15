e5f0b8719ec7b0a68c9ba2214bdd14115d13fc43
PacketVideo CM <engbuild@pv.com>
2010-04-15 13:34:32 -0700

    RIO-8930: Add a target in module level unit test to display the list of available targets
    Change-Id: I24841ff79ba071e038a4cef38cfcb2b9d1d12d3f

--- engines/2way/src/pv_2way_sdkinfo.h
+++ engines/2way/src/pv_2way_sdkinfo.h
@@ -21,7 +21,7 @@
 // This header file is automatically generated at build-time
 // *** OFFICIAL RELEASE INFO -- Will not auto update
 
-#define PV2WAY_ENGINE_SDKINFO_LABEL "1362940"
-#define PV2WAY_ENGINE_SDKINFO_DATE 0x20100406
+#define PV2WAY_ENGINE_SDKINFO_LABEL "1363430"
+#define PV2WAY_ENGINE_SDKINFO_DATE 0x20100407
 
 #endif //PV_2WAY_SDKINFO_H_INCLUDED
--- engines/player/src/pv_player_sdkinfo.h
+++ engines/player/src/pv_player_sdkinfo.h
@@ -21,7 +21,7 @@
 // This header file is automatically generated at build-time
 // *** OFFICIAL RELEASE INFO -- Will not auto update
 
-#define PVPLAYER_ENGINE_SDKINFO_LABEL "1362940"
-#define PVPLAYER_ENGINE_SDKINFO_DATE 0x20100406
+#define PVPLAYER_ENGINE_SDKINFO_LABEL "1363430"
+#define PVPLAYER_ENGINE_SDKINFO_DATE 0x20100407
 
 #endif //PV_PLAYER_SDKINFO_H_INCLUDED
