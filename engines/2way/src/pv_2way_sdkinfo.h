71829da6aa836bad7901241b125ff15f3cbff9bd
PacketVideo CM <engbuild@pv.com>
2010-04-15 13:34:30 -0700

    RIO-8940: Mandatory directory for 2way tests is not spelled correctly
    Change-Id: I00cdb076766648f535dc17c876a5e3b00a44232e

--- engines/2way/src/pv_2way_sdkinfo.h
+++ engines/2way/src/pv_2way_sdkinfo.h
@@ -21,7 +21,7 @@
 // This header file is automatically generated at build-time
 // *** OFFICIAL RELEASE INFO -- Will not auto update
 
-#define PV2WAY_ENGINE_SDKINFO_LABEL "1359188"
-#define PV2WAY_ENGINE_SDKINFO_DATE 0x20100405
+#define PV2WAY_ENGINE_SDKINFO_LABEL "1361485"
+#define PV2WAY_ENGINE_SDKINFO_DATE 0x20100406
 
 #endif //PV_2WAY_SDKINFO_H_INCLUDED
--- engines/player/src/pv_player_sdkinfo.h
+++ engines/player/src/pv_player_sdkinfo.h
@@ -21,7 +21,7 @@
 // This header file is automatically generated at build-time
 // *** OFFICIAL RELEASE INFO -- Will not auto update
 
-#define PVPLAYER_ENGINE_SDKINFO_LABEL "1359188"
-#define PVPLAYER_ENGINE_SDKINFO_DATE 0x20100405
+#define PVPLAYER_ENGINE_SDKINFO_LABEL "1361485"
+#define PVPLAYER_ENGINE_SDKINFO_DATE 0x20100406
 
 #endif //PV_PLAYER_SDKINFO_H_INCLUDED
