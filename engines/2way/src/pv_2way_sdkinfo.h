b12b6916a47baf3133911648c73239afe454db1f
PacketVideo CM <engbuild@pv.com>
2010-04-15 13:34:44 -0700

    CORE_8.509.1.3 versioning and documentation updates.
    Change-Id: I70b0ce5e67665d04393ece21b59876c40c5b875c

--- engines/2way/src/pv_2way_sdkinfo.h
+++ engines/2way/src/pv_2way_sdkinfo.h
@@ -21,7 +21,7 @@
 // This header file is automatically generated at build-time
 // *** OFFICIAL RELEASE INFO -- Will not auto update
 
-#define PV2WAY_ENGINE_SDKINFO_LABEL "1370330"
+#define PV2WAY_ENGINE_SDKINFO_LABEL "CORE_8.509.1.3"
 #define PV2WAY_ENGINE_SDKINFO_DATE 0x20100409
 
 #endif //PV_2WAY_SDKINFO_H_INCLUDED
