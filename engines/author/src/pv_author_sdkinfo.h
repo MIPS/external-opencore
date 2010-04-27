84adaec5ca247a88668ab81aa7e19aec066f5735
PacketVideo CM <engbuild@pv.com>
2010-04-27 11:02:33 -0700

    RIO-9044: Change test content: test_adts_x.pcm
    Change-Id: I2613ff5cb8615b9cfdb1ee4ecebb0f349724fe3f

--- engines/author/src/pv_author_sdkinfo.h
+++ engines/author/src/pv_author_sdkinfo.h
@@ -21,7 +21,7 @@
 // This header file is automatically generated at build-time
 // *** OFFICIAL RELEASE INFO -- Will not auto update
 
-#define PVAUTHOR_ENGINE_SDKINFO_LABEL "1395171"
-#define PVAUTHOR_ENGINE_SDKINFO_DATE 0x20100422
+#define PVAUTHOR_ENGINE_SDKINFO_LABEL "1397022"
+#define PVAUTHOR_ENGINE_SDKINFO_DATE 0x20100423
 
 #endif //PV_AUTHOR_SDKINFO_H_INCLUDED
--- engines/player/src/pv_player_sdkinfo.h
+++ engines/player/src/pv_player_sdkinfo.h
@@ -21,7 +21,7 @@
 // This header file is automatically generated at build-time
 // *** OFFICIAL RELEASE INFO -- Will not auto update
 
-#define PVPLAYER_ENGINE_SDKINFO_LABEL "1395171"
-#define PVPLAYER_ENGINE_SDKINFO_DATE 0x20100422
+#define PVPLAYER_ENGINE_SDKINFO_LABEL "1397022"
+#define PVPLAYER_ENGINE_SDKINFO_DATE 0x20100423
 
 #endif //PV_PLAYER_SDKINFO_H_INCLUDED
