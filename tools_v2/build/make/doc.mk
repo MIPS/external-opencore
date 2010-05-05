d192e9c0d6e81e6ac79b2d66e8b8b3b8d0cf2b96
PacketVideo CM <engbuild@pv.com>
2010-05-05 14:13:38 -0700

    RIO-9038: Introduce new "open" option for API documentation generation
    Change-Id: Ie14c5f20b30afd08ceeb66a3e43aa723ebb91a4f

--- tools_v2/build/make/doc.mk
+++ tools_v2/build/make/doc.mk
@@ -11,5 +11,9 @@ doc_$(doc_target): doc_rule_title := $(doc_title)
 doc_$(doc_target): doc_rule_paths := $(doc_paths)
 doc_$(doc_target): doc_rule_version := $(doc_version)
 
+ifeq ($(strip $(DEFAULT_DOCMODE)),open)
+	doc_open_option := --open
+endif
+
 doc_$(doc_target):
-	perl $(SRC_ROOT)/tools_v2/build/document/bin/doc_build.bat --doctype $(doc_rule_target) --title $(doc_rule_title) --path $(doc_rule_paths) --filetype "mainpage *.h readme.txt" --exclude_pattern "*/test/* */obsolete/* */doxydir/*" -ver $(doc_rule_version)
+	perl $(SRC_ROOT)/tools_v2/build/document/bin/doc_build.bat --doctype $(doc_rule_target) --title $(doc_rule_title) --path $(doc_rule_paths) --filetype "mainpage *.h readme.txt" --exclude_pattern "*/test/* */obsolete/* */doxydir/*" -ver $(doc_rule_version) $(doc_open_option)
