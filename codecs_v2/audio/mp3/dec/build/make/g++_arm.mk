a464c7c3ea1cd02f844fe07486b7df001ed6c63d
PacketVideo CM <engbuild@pv.com>
2010-05-11 10:39:19 -0700

    RIO-9064: Improve the check for GLOBAL_CPU_ARCH_VERSION in codecs lib makefile to make it scalable to higher version of ARM architecture.
    Change-Id: Ic26e430993c2befac410401857489f94b3699330

--- codecs_v2/audio/mp3/dec/build/make/g++_arm.mk
+++ codecs_v2/audio/mp3/dec/build/make/g++_arm.mk
@@ -1,13 +1,16 @@
 # Additional assembly files for linux-arm
-ifeq ($(GLOBAL_CPU_ARCH_VERSION),4)
-    ASM_INCLUSION:=1
-else ifeq ($(GLOBAL_CPU_ARCH_VERSION),5)
-    ASM_INCLUSION:=1
-else ifeq ($(GLOBAL_CPU_ARCH_VERSION),6)
-    ASM_INCLUSION:=1
-else
+ifeq ($(GLOBAL_CPU_ARCH_VERSION),0)
     ASM_INCLUSION:=0
+else ifeq ($(GLOBAL_CPU_ARCH_VERSION),1)
+    ASM_INCLUSION:=0
+else ifeq ($(GLOBAL_CPU_ARCH_VERSION),2)
+    ASM_INCLUSION:=0
+else ifeq ($(GLOBAL_CPU_ARCH_VERSION),3)
+    ASM_INCLUSION:=0
+else
+    ASM_INCLUSION:=1
 endif
+
 ifneq ($(ASM_INCLUSION),0)
 SRCS+=  asm/pvmp3_polyphase_filter_window_gcc.s \
         asm/pvmp3_mdct_18_gcc.s \
--- engines/2way/src/pv_2way_sdkinfo.h
+++ engines/2way/src/pv_2way_sdkinfo.h
@@ -21,7 +21,7 @@
 // This header file is automatically generated at build-time
 // *** OFFICIAL RELEASE INFO -- Will not auto update
 
-#define PV2WAY_ENGINE_SDKINFO_LABEL "1412929"
+#define PV2WAY_ENGINE_SDKINFO_LABEL "1413764"
 #define PV2WAY_ENGINE_SDKINFO_DATE 0x20100503
 
 #endif //PV_2WAY_SDKINFO_H_INCLUDED
