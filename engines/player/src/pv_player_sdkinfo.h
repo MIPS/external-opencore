c30172c444e090d62297816418f83d3a27f88c6d
PacketVideo CM <engbuild@pv.com>
2010-05-17 11:14:29 -0700

    RIO-9123: Change implementation of XML escape() for CodeSonar memory leak analysis
    Change-Id: I122d05b4502ec90247d2530014b7abe7b4de90fd

--- engines/player/src/pv_player_sdkinfo.h
+++ engines/player/src/pv_player_sdkinfo.h
@@ -21,7 +21,7 @@
 // This header file is automatically generated at build-time
 // *** OFFICIAL RELEASE INFO -- Will not auto update
 
-#define PVPLAYER_ENGINE_SDKINFO_LABEL "1431284"
+#define PVPLAYER_ENGINE_SDKINFO_LABEL "1432338"
 #define PVPLAYER_ENGINE_SDKINFO_DATE 0x20100512
 
 #endif //PV_PLAYER_SDKINFO_H_INCLUDED
