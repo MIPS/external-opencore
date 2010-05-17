f3e636f7dc6f9eb46ded9ffe7df9a4cb69f8baaf
PacketVideo CM <engbuild@pv.com>
2010-05-17 11:14:44 -0700

    CORE_9.000.1.1_RC3 versioning and documentation updates.
    Change-Id: I4e4d293b795596a40f16bf18f8ddefa5cf09e9b8

--- engines/player/src/pv_player_sdkinfo.h
+++ engines/player/src/pv_player_sdkinfo.h
@@ -21,7 +21,7 @@
 // This header file is automatically generated at build-time
 // *** OFFICIAL RELEASE INFO -- Will not auto update
 
-#define PVPLAYER_ENGINE_SDKINFO_LABEL "1438294"
+#define PVPLAYER_ENGINE_SDKINFO_LABEL "CORE_9.000.1.1_RC3"
 #define PVPLAYER_ENGINE_SDKINFO_DATE 0x20100514
 
 #endif //PV_PLAYER_SDKINFO_H_INCLUDED
