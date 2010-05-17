cf24d170d1a9ec588c6517e940ab7261d8c35202
PacketVideo CM <engbuild@pv.com>
2010-05-17 11:14:28 -0700

    RIO-9094: Fix BOS and EOS timestamps for MP3
    Change-Id: I18561a32c56c9e948711176b0ceb258db01c5a57

--- engines/player/src/pv_player_sdkinfo.h
+++ engines/player/src/pv_player_sdkinfo.h
@@ -21,7 +21,7 @@
 // This header file is automatically generated at build-time
 // *** OFFICIAL RELEASE INFO -- Will not auto update
 
-#define PVPLAYER_ENGINE_SDKINFO_LABEL "1430904"
+#define PVPLAYER_ENGINE_SDKINFO_LABEL "1431077"
 #define PVPLAYER_ENGINE_SDKINFO_DATE 0x20100511
 
 #endif //PV_PLAYER_SDKINFO_H_INCLUDED
