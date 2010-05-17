edca7ad660950d1fd796e8311f3fa11b46d5a5ee
PacketVideo CM <engbuild@pv.com>
2010-05-17 11:14:33 -0700

    RIO-9165: Incorrect duration for AAC ADIF file
    Change-Id: I92fcd8231a7f7ff4ec493a34ef299460d4a39391

--- engines/author/src/pv_author_sdkinfo.h
+++ engines/author/src/pv_author_sdkinfo.h
@@ -21,7 +21,7 @@
 // This header file is automatically generated at build-time
 // *** OFFICIAL RELEASE INFO -- Will not auto update
 
-#define PVAUTHOR_ENGINE_SDKINFO_LABEL "1434032"
+#define PVAUTHOR_ENGINE_SDKINFO_LABEL "1435339"
 #define PVAUTHOR_ENGINE_SDKINFO_DATE 0x20100513
 
 #endif //PV_AUTHOR_SDKINFO_H_INCLUDED
