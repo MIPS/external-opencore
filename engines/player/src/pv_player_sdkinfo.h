dff06cb06f68b149f840f7f6a9bbc621c9b16087
PacketVideo CM <engbuild@pv.com>
2010-06-15 16:35:57 -0700

    RIO-8608: Cleaned up DRM plugin interface
    Change-Id: I9c1697169cb894d68848b0c3a73a7bcbd524b933

--- engines/player/src/pv_player_sdkinfo.h
+++ engines/player/src/pv_player_sdkinfo.h
@@ -21,7 +21,7 @@
 // This header file is automatically generated at build-time
 // *** OFFICIAL RELEASE INFO -- Will not auto update
 
-#define PVPLAYER_ENGINE_SDKINFO_LABEL "1467301"
+#define PVPLAYER_ENGINE_SDKINFO_LABEL "1467359"
 #define PVPLAYER_ENGINE_SDKINFO_DATE 0x20100528
 
 #endif //PV_PLAYER_SDKINFO_H_INCLUDED
