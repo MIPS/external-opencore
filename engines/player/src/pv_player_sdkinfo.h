5db262cd7b42a771493119c728aae0f5cc52eb54
PacketVideo CM <engbuild@pv.com>
2010-06-15 16:36:01 -0700

    RIO-7793: Modify AVCConfigParser to handle multiple SPSs/PPSs, AUD and SEI NALs.
    Change-Id: I05605df9e4b469ad4ac160628afd58276def7fd1

--- engines/player/src/pv_player_sdkinfo.h
+++ engines/player/src/pv_player_sdkinfo.h
@@ -21,7 +21,7 @@
 // This header file is automatically generated at build-time
 // *** OFFICIAL RELEASE INFO -- Will not auto update
 
-#define PVPLAYER_ENGINE_SDKINFO_LABEL "1482739"
+#define PVPLAYER_ENGINE_SDKINFO_LABEL "1482780"
 #define PVPLAYER_ENGINE_SDKINFO_DATE 0x20100607
 
 #endif //PV_PLAYER_SDKINFO_H_INCLUDED
