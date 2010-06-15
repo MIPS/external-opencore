dc53d1a0d75799755fbf4efc6f557b9bc9fc2ecc
PacketVideo CM <engbuild@pv.com>
2010-06-15 16:36:14 -0700

    RIO-9185: CommonParserNode for AMR and WAV (minor additional change 5)
    Change-Id: Id9358e50d06f94c7b81fb21a2dc487e8cc253bac

--- engines/author/src/pv_author_sdkinfo.h
+++ engines/author/src/pv_author_sdkinfo.h
@@ -21,7 +21,7 @@
 // This header file is automatically generated at build-time
 // *** OFFICIAL RELEASE INFO -- Will not auto update
 
-#define PVAUTHOR_ENGINE_SDKINFO_LABEL "1493122"
+#define PVAUTHOR_ENGINE_SDKINFO_LABEL "1493239"
 #define PVAUTHOR_ENGINE_SDKINFO_DATE 0x20100611
 
 #endif //PV_AUTHOR_SDKINFO_H_INCLUDED
