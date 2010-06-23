d7c1068f7c2044aacde3922dec25d76860847887
PacketVideo CM <engbuild@pv.com>
2010-06-23 14:07:00 -0700

    CORE_9.004.1.1 versioning and documentation updates.
    Change-Id: Icc6237a565b48d80705f6305e4fda00db41ecfd9

--- engines/2way/src/pv_2way_sdkinfo.h
+++ engines/2way/src/pv_2way_sdkinfo.h
@@ -21,7 +21,7 @@
 // This header file is automatically generated at build-time
 // *** OFFICIAL RELEASE INFO -- Will not auto update
 
-#define PV2WAY_ENGINE_SDKINFO_LABEL "1511188"
-#define PV2WAY_ENGINE_SDKINFO_DATE 0x20100622
+#define PV2WAY_ENGINE_SDKINFO_LABEL "CORE_9.004.1.1"
+#define PV2WAY_ENGINE_SDKINFO_DATE 0x20100623
 
 #endif //PV_2WAY_SDKINFO_H_INCLUDED
--- engines/player/src/pv_player_sdkinfo.h
+++ engines/player/src/pv_player_sdkinfo.h
@@ -21,7 +21,7 @@
 // This header file is automatically generated at build-time
 // *** OFFICIAL RELEASE INFO -- Will not auto update
 
-#define PVPLAYER_ENGINE_SDKINFO_LABEL "1511188"
-#define PVPLAYER_ENGINE_SDKINFO_DATE 0x20100622
+#define PVPLAYER_ENGINE_SDKINFO_LABEL "CORE_9.004.1.1"
+#define PVPLAYER_ENGINE_SDKINFO_DATE 0x20100623
 
 #endif //PV_PLAYER_SDKINFO_H_INCLUDED
