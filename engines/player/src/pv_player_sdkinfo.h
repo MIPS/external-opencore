7d1a8921dd4fae5928e47fa49cee402888bc3b0e
PacketVideo CM <engbuild@pv.com>
2010-06-28 11:07:53 -0700

    CORE_9.005.1.1 versioning and documentation updates.
    Change-Id: I49936cb7547430e346a85cbf0b12fb0a175ed709

--- engines/player/src/pv_player_sdkinfo.h
+++ engines/player/src/pv_player_sdkinfo.h
@@ -21,7 +21,7 @@
 // This header file is automatically generated at build-time
 // *** OFFICIAL RELEASE INFO -- Will not auto update
 
-#define PVPLAYER_ENGINE_SDKINFO_LABEL "1518416"
+#define PVPLAYER_ENGINE_SDKINFO_LABEL "CORE_9.005.1.1"
 #define PVPLAYER_ENGINE_SDKINFO_DATE 0x20100625
 
 #endif //PV_PLAYER_SDKINFO_H_INCLUDED
